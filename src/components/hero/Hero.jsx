af597967adc4dbb9283071e478eff43e279e5560
antonellalm <antonellarios0098@gmail.com>
2023-11-16 14:31:21 -0300

    utlimos cambios

---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,36 @@
+import "./Hero.scss";
+import Wrapper from "../wrapper/wrapper";
+import Img1 from "../../assets/hero-1.png";
+import Img2 from "../../assets/hero-2.png";
+import Img3 from "../../assets/hero-3.png";
+export default function Hero() {
+  return (
+    <section className="hero">
+      <Wrapper className="hero__container">
+        <div className="hero__left">
+          <h1>Make payment easy,simplify your finance</h1>
+          <p>
+            Explained propriety off out perpetual his you. Fell sold off felt
+            nay rose met you.We so entreties cultivated astonished is. Offered
+            chiefly father of my colonel shynesjj.Such on help ye some door if
+            in
+          </p>
+          <div className="hero__btn-container">
+            <a href="#" className="button-primary">
+              {" "}
+              Get Started - it is free
+            </a>
+            <a href="" className="button-outline">
+              How it's works
+            </a>
+          </div>
+        </div>
+        <div className="hero__right">
+          <img src={Img1} alt="" className="img-1" />
+          <img src={Img2} alt="" className="img-2" />
+          <img src={Img3} alt="" className="img-3" />
+        </div>
+      </Wrapper>
+    </section>
+  );
+}
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,3 +1,41 @@
+import "./Navbar.scss";
+import Wrapper from "../wrapper/wrapper";
+import Logo from "../../assets/logo.svg";
+import { FaBars } from "react-icons/fa";
+import { useState } from "react";
 export default function Navbar() {
-  return <div>nabvbar</div>;
+  const [showNav, setShowNav] = useState(false);
+  return (
+    <nav className="navbar">
+      <Wrapper className="navbar__container">
+        <a href="#" className="navbar__logo" onClick={() => setShowNav(false)}>
+          <img src={Logo}></img>
+        </a>
+        <ul className={`navbar__links ${showNav ? "show-nav" : ""}`}>
+          <li onClick={() => setShowNav(false)}>
+            <a href="#">Home</a>
+          </li>
+          <li onClick={() => setShowNav(false)}>
+            <a href="#">About</a>
+          </li>
+          <li onClick={() => setShowNav(false)}>
+            <a href="#">Pricing</a>
+          </li>
+          <li onClick={() => setShowNav(false)}>
+            <a href="#">Blog</a>
+          </li>
+        </ul>
+        <a href="" className="button-primary navbar__btn">
+          {" "}
+          Sign Up
+        </a>
+        <div
+          className={`navbar__menubar ${showNav ? "bg-color" : ""}`}
+          onClick={() => setShowNav(!showNav)}
+        >
+          <FaBars />
+        </div>
+      </Wrapper>
+    </nav>
+  );
 }
