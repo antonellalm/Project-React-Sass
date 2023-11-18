eb9cedd12b01ab112d55064c115d67a7c42b97ff
antonellalm <antonellarios0098@gmail.com>
2023-11-18 19:08:20 -0300

    paymentSection

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import Navbar from "./components/navbar/Navbar";
 import Hero from "./components/hero/Hero";
-
+import CompanySection from "./components/companySection/CompanySection";
+import FeaturesSection from "./components/featuresSection/FeaturesSection";
+import PaymentSection from "./components/paymentSection/PaymentSection";
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <CompanySection />
+      <FeaturesSection />
+      <PaymentSection />
     </>
   );
 }
